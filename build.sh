9076ad34c672ac8931882679a0d1cb314c2ecc96
Raiane Dev <89032013+Raiane-Dev@users.noreply.github.com>
2022-04-02 13:00:37 -0300

    Mudando váriaveis de ambiente dinâmicamente.

--- build.sh
+++ build.sh
@@ -2,11 +2,28 @@ composer install ;
 
 composer update ;
 
+echo "DB_DATABASE:" ;
+read db_database ;
+
+echo "DB_USERNAME:" ;
+read db_username ;
+
+echo "DB_PASSWORD: " ;
+read db_password ;
+
+arg_database="DB_DATABASE="$db_database ;
+arg_username="DB_USERNAME="$db_username ;
+arg_password"DB_PASSWORD="$db_password ;
+
+echo $arg_database >> ./.env.example ;
+echo $arg_username >> ./.env.example ;
+echo $arg_password >> ./.env.example ;
+
 cp ./.env.example ./.env ;
 
 php artisan migrate ;
 
-php artisan make:migration fpay
+php artisan make:migration fpay ;
 
 # Vamos rodar!
 php -S localhost:8000 -t public
