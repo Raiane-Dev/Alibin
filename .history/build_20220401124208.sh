4d2242487dfe5c1f800dfcf45850f472a580087b
Raiane Dev <89032013+Raiane-Dev@users.noreply.github.com>
2022-04-01 23:48:51 -0300

    Deletando histórico (deveria estar no .gitgnore)

--- .history/build_20220401124208.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-# composer install ;
-# composer update ;
-# php artisan migrate ;
-# php artisan make:migration fpay
-
-# Vamos rodar!
-# php -S localhost:8000 -t public
--- .history/build_20220401214035.sh
+++
@@ -1,8 +0,0 @@
-composer install ;
-
-composer update ;
-php artisan migrate ;
-php artisan make:migration fpay
-
-# Vamos rodar!
-php -S localhost:8000 -t public
